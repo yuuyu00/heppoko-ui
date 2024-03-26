b499be9f2408db70566716a826d61dfa33484f23
yuuyu00 <my219813@gmail.com>
2024-03-26 20:10:39 +0900

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES ? "heppoko-ui" : "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
